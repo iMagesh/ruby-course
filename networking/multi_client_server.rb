2b888b873c376ad2db23784722b1b644214977c3
Magesh <magesh@hash14.com>
2018-10-08 01:07:07 +0530

    Added networking sample codes client, server, multi-client, ruby browser, web_client

---
+++ networking/multi_client_server.rb
@@ -0,0 +1,11 @@
+require 'socket'                 # Get sockets from stdlib
+
+server = TCPServer.open(2000)    # Socket to listen on port 2000
+loop {                           # Servers run forever
+  Thread.start(server.accept) do |client|
+    client.puts(client.inspect)
+    client.puts(Time.now.ctime)   # Send the time to the client
+    client.puts "Closing the connection. Bye!"
+    client.close                  # Disconnect from the client
+  end
+}
---
+++ networking/web_client.rb
@@ -0,0 +1,11 @@
+require 'net/http'                  # The library we need
+host = 'www.tutorialspoint.com'     # The web server
+path = '/index.htm'                 # The file we want
+
+http = Net::HTTP.new(host)          # Create a connection
+headers, body = http.get(path)      # Request the file
+if headers.code == "200"            # Check the status code
+  puts headers.body
+else
+  puts "#{headers.code} #{headers.message}"
+end
